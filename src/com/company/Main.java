90baefc3cf564a1931820fab33a0c04119267117
Just-ich <edik01101997@mail.ru>
2022-02-20 12:28:20 +0300

    erst

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int age=25;
+        age--;
+        int q;
+        System.out.println("Привет\nМне "+age+" лет.");
+
+
+    }
+}
