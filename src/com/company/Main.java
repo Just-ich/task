e2e98432142fd4352baf0ad36f139ac863d815d5
Just-ich <edik01101997@mail.ru>
2022-02-26 18:47:09 +0300

    Task1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,55 @@
 package com.company;
 
 
+import com.sun.jdi.IntegerValue;
+import com.sun.jdi.connect.Connector;
+
 public class Main {
 
     public static void main(String[] args) {
+// 1
+        Double c = Double.valueOf(5);
+        System.out.println(c);
 
-        int age=25;
-        age--;
-        int q;
-        System.out.println("Привет\nМне "+age+" лет.");
-        System.out.println("Hello Java");
-        System.out.println("Всё, пока.");
+// 2
+        String s = String.valueOf(80);
+        Integer i = Integer.parseInt(s);
+        System.out.println(i);
 
 
+// 3
+        int q =Integer.parseInt("1100001001", 2);
+        System.out.println(q);
+
+// 4
+        String w = String.valueOf(1234567890);
+        Integer w1 = Integer.parseInt(w);
+        System.out.println(w1);
+
+// 5
+        System.out.println(Integer.MAX_VALUE);
+
+// 6
+        System.out.println(Integer.MIN_VALUE);
+
+// 7
+        System.out.println(Byte.MAX_VALUE);
+
+// 8
+        System.out.println(Byte.MIN_VALUE+"\n");
+
+// 9
+        String str = "Привет группа 132";
+        String strUpper = str.toUpperCase();
+        System.out.println("Оригинал строки: " + str);
+        System.out.println("Изменение строки в верхний регистр: " + strUpper+"\n");
+
+// 10
+        String Str1 = new String("Привет группа 132");
+        System.out.print("Длина строки \"Привет группа 132\" - " + Str1.length()+ " \n" );
+
+// 11
+        str = strUpper.replaceAll(" (.{1})", "$1 ");
+        System.out.println("\n"+str);
     }
 }
